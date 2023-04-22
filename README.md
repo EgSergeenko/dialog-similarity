e8420c8b9fd6f1fe7b4082429fff0ac0244d5e24
Egor Sergeenko <es.egor.sergeenko@gmail.com>
2023-04-22 13:11:46 +0300

    Init commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Dialog similarity
+
+* Python 3.10
+* Download [dataset](https://dax-cdn.cdn.appdomain.cloud/dax-conversation-similarity/1.0.0/data_preview/index.html) `python download_dataset.py`
---
+++ download_dataset.py
@@ -0,0 +1,29 @@
+import os
+import tarfile
+
+import requests
+
+DATASET_URL = (
+    'https://dax-cdn.cdn.appdomain.cloud/dax-conversation-similarity/'
+    '1.0.0/conversation-similarity.tar.gz'
+)
+
+
+def download_dataset():
+    response = requests.get(DATASET_URL)
+    archive_filename = 'tmp.tar.gz'
+    with open(archive_filename, 'wb') as archive_file:
+        archive_file.write(response.content)
+
+    with tarfile.open(archive_filename) as archive_file:
+        members = [
+            member for member in archive_file.getmembers()
+            if not member.name.startswith('.')
+        ]
+        archive_file.extractall(members=members)
+
+    os.remove(archive_filename)
+
+
+if __name__ == '__main__':
+    download_dataset()

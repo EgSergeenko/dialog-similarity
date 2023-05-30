fa8e47dedfa2524a5d0a09e8034c8ee3c6e17868
Egor Sergeenko <es.egor.sergeenko@gmail.com>
2023-05-30 13:49:57 +0300

    Add augmentations for dataset

--- data.py
+++ data.py
@@ -1,16 +1,67 @@
 import json
 import os
+from copy import deepcopy
 
-from sentence_transformers import SentenceTransformer
+import torch
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+from torch.utils.data import Dataset
 from tqdm import tqdm
 
-from dialog import dialog_from_dict, Dialog
+from dialog import Dialog, dialog_from_dict
+from model import Embedder
 
 
-class SGDDataset(object):
-    def __init__(self, dataset_dir: str) -> None:
+class TranslationModel(object):
+    def __init__(
+        self,
+        model_name: str,
+        device: str,
+        num_beams: int = 3,
+        num_return_utterances: int =1,
+    ) -> None:
+        if not torch.cuda.is_available() and 'cuda' in device:
+            device = 'cpu'
+        self.device = torch.device(device)
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+        self.model = AutoModelForSeq2SeqLM.from_pretrained(model_name).to(
+            self.device,
+        )
+        self.num_beams = num_beams
+        self.num_return_utterances = num_return_utterances
+
+    def __call__(self, texts: list[str]) -> list[str]:
+        tokens = self.tokenizer(texts, return_tensors='pt', padding=True)
+        outputs = self.model.generate(
+            input_ids=tokens['input_ids'].to(self.device),
+            attention_mask=tokens['attention_mask'].to(self.device),
+            num_beams=self.num_beams,
+            num_return_sequences=self.num_return_utterances,
+        )
+        return self.tokenizer.batch_decode(
+            outputs, skip_special_tokens=True,
+        )
+
+
+class AugmentationModel(object):
+    def __init__(self, device: str):
+        self.en_de_model = TranslationModel(
+            'Helsinki-NLP/opus-mt-en-de', device,
+        )
+        self.de_en_model = TranslationModel(
+            'Helsinki-NLP/opus-mt-de-en', device,
+        )
+
+    def __call__(self, texts: list[str]) -> list[str]:
+        texts = self.en_de_model(texts)
+        return self.de_en_model(texts)
+
+
+class SGDDataset(Dataset):
+    def __init__(self, dataset_dir: str, limit: int) -> None:
+        self.aug_subdir = 'aug'
         self.dataset_dir = dataset_dir
-        self.dialogs = self._preprocess_dataset()
+        self.dialogs = self._load_dialogs()[:limit]
+        self.augmented_dialogs = self._augment_dialogs()
         self.id_2_idx = self._create_mapping()
 
     def __getitem__(self, idx: int) -> Dialog:
@@ -22,11 +73,16 @@ class SGDDataset(object):
     def compute_embeddings(
         self,
         cache_dir: str,
-        model: SentenceTransformer,
+        model: Embedder,
         model_name: str,
     ) -> None:
-        for dialog in tqdm(self.dialogs):
-            dialog.compute_embeddings(cache_dir, model, model_name)
+        for idx in tqdm(range(len(self.dialogs)), desc='Computing embeddings...'):
+            self.dialogs[idx].compute_embeddings(
+                cache_dir, model, model_name,
+            )
+            self.augmented_dialogs[idx].compute_embeddings(
+                cache_dir, model, model_name,
+            )
 
     def load_clusters(self, clustering_filepath: str) -> None:
         with open(clustering_filepath) as clustering_file:
@@ -42,7 +98,7 @@ class SGDDataset(object):
             id_2_idx[dialog.dialog_id] = idx
         return id_2_idx
 
-    def _preprocess_dataset(self) -> list[Dialog]:
+    def _load_dialogs(self) -> list[Dialog]:
         sub_dirs = ['train']
 
         dialogs_filepaths = []
@@ -62,3 +118,46 @@ class SGDDataset(object):
                     dialog = dialog_from_dict(dialog_dict)
                     dialogs.append(dialog)
         return dialogs
+
+    def _augment_dialogs(self) -> list[Dialog]:
+        augmented_dialogs = []
+        os.makedirs(
+            os.path.join(self.dataset_dir, self.aug_subdir),
+            exist_ok=True,
+        )
+
+        model = AugmentationModel('cuda')
+
+        for dialog in tqdm(self.dialogs, desc='Augmenting...'):
+            augmented_dialogs.append(
+                self._augment_dialog(deepcopy(dialog), model),
+            )
+
+        del model
+
+        return augmented_dialogs
+
+    def _augment_dialog(
+        self,
+        dialog: Dialog,
+        model: AugmentationModel,
+    ) -> Dialog:
+        dialog.dialog_id = '{0}_aug'.format(dialog.dialog_id)
+        dialog_filepath = os.path.join(
+            self.dataset_dir,
+            self.aug_subdir,
+            '{0}.json'.format(dialog.dialog_id),
+        )
+
+        if os.path.isfile(dialog_filepath):
+            with open(dialog_filepath) as dialog_file:
+                utterances = json.load(dialog_file)
+        else:
+            utterances = model([turn.utterance for turn in dialog.turns])
+            with open(dialog_filepath, 'w') as dialog_file:
+                json.dump(utterances, dialog_file)
+
+        for idx in range(len(utterances)):
+            dialog.turns[idx].utterance = utterances[idx]
+
+        return dialog
